a8c995c9196a7e95352bc04ef82eb18280c2ec77
Shubham Yadav <106825181+Shubham-Yadav-18@users.noreply.github.com>
2023-09-06 12:57:35 +0000

    memory heap and stack

---
+++ js/7_StackAndHeapMemory.js
@@ -0,0 +1,16 @@
+ let names="Shubham";
+ let childName=names;
+ childName="shubh";
+ console.log(names);
+ console.log(childName);
+
+//  non premitive
+let obj={
+    email:"shubh@ds",
+    address:"sdsds"
+
+}
+let obj2=obj;
+obj2.email="NEwEmail";
+console.log(obj.email);
+console.log(obj2.email);
