be808292edddcef68e56d399d26febbf7e33f9f7
Shubham Yadav <106825181+Shubham-Yadav-18@users.noreply.github.com>
2023-09-06 11:30:29 +0000

    conversion done

---
+++ js/2_variables.js
@@ -0,0 +1,4 @@
+const id=23;
+// id=34;
+let email="sdlfe";
+console.log(email + "/n"+ id);
---
+++ js/4_conversion.js
@@ -0,0 +1,22 @@
+let marks="45a"
+console.log(typeof marks);
+
+let valueInNumber=Number(marks); 
+// js is case senstive
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// conversion in boolean
+
+let islogedIn=2;
+let booleanIsLoggedIn=Boolean(islogedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0 =>false;
+//  ""=> false;
+// "dckl" =>true; 
+let str="334";
+let num=Number(str);
+console.log(typeof num);
+console.log(num);
